d0a3eaeed76344b219aa87ef1f7aa489a1a8077c
Brady Svedin <brady@lightningkite.com>
2024-09-27 14:51:18 -0600

    Kotlin version increase

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
-        classpath("com.lightningkite:deploy-helpers:0.0.4")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
+        classpath("com.lightningkite:deploy-helpers:0.0.7")
         // Add the Crashlytics Gradle plugin (be sure to add version
         // 2.0.0 or later if you built your app with Android Studio 4.1).
     }
